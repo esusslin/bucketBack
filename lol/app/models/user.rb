772c2e0ba54d6d196017e6fb99e6e25480a93cd2
Emmet Susslin <emmet@Emmets-MacBook-Pro.local>
2017-06-07 19:54:38 -0700

    lol

--- lol/app/models/user.rb
+++ lol/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
 	has_many :proposals
-  	validates :first_name, presence: true,
-                    length: { minimum: 5 }
+  	# validates :first_name, presence: true,
+   #                  length: { minimum: 5 }
 end
